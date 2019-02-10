6d654ee47c0fc1c11fdbbc42fe123cd7916b46b9
Dawid.Rym <dawid.jscript@gmail.com>
2019-02-10 20:16:20 +0100

    Initial commit

---
+++ api/models/order-schema.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+const books = require('../files/books');
+
+const orderSchema = Joi.object().options({ abortEarly: false }).keys({
+  order: Joi.array().min(1).items(Joi.object().keys({
+    id: Joi.number().valid(books.map(book => book.id)).required(),
+    quantity: Joi.number().min(1).required(),
+  })).required(),
+  first_name: Joi.string().required().min(4).max(50),
+  last_name: Joi.string().required().min(5).max(50),
+  email: Joi.string().required().email().max(100),
+  phone_number: Joi.number().required().min(500000000).max(889999999),
+  address: Joi.string().required(),
+  city: Joi.string().required(),
+  zip_code: Joi.string().required().regex(/\d{2}-\d{3}/),
+});
+
+module.exports = orderSchema;
