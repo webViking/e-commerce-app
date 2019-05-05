3ed9ab409d0a4aeb5bf6f1e772b48daac1749db5
Dawid.Rym <dawid.jscript@gmail.com>
2019-05-05 20:08:57 +0200

    Set up a project

---
+++ front/src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+
+  render() {
+    return (
+        <div className ="container mt-3">
+
+        </div>
+    );
+  }
+}
+
+export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
-import App from './App';
+import App from './containers/App';
+
+
 
 dotenv.config();
 ReactDOM.render(<App />, document.getElementById('root'));
